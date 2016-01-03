8b6c5c70c6c4d48c67e06d786ca6c8ffaaa8ae5a
James Swineson <jamesswineson@gmail.com>
2016-01-03 23:32:46 +0800

    remove undef's

--- ctrycatch.h
+++ ctrycatch.h
@@ -32,41 +32,20 @@
 #include <stdbool.h>
 
 // Some macro magic
-#ifdef CAT
-#undef CAT
-#endif
-#define CAT(a, ...) PRIMITIVE_CAT(a, __VA_ARGS__)
-#ifdef PRIMITIVE_CAT
-#undef PRIMITIVE_CAT
-#endif
-#define PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
+#define CTRYCATCH_CAT(a, ...) CTRYCATCH_PRIMITIVE_CAT(a, __VA_ARGS__)
+#define CTRYCATCH_PRIMITIVE_CAT(a, ...) a ## __VA_ARGS__
 
-#ifdef CTRYCATCH_NAME
-#undef CTRYCATCH_NAME
-#endif
-#define CTRYCATCH_NAME(X) CAT(__ctrycatch_, X)
+#define CTRYCATCH_NAME(X) CTRYCATCH_CAT(__ctrycatch_, X)
 
 // New block arguments
-#ifdef try
-#undef try
-#endif
 #define try \
     if(!(CTRYCATCH_NAME(exception_type) = setjmp(CTRYCATCH_NAME(exception_env))))
 
-#ifdef catch
-#undef catch
-#endif
 #define catch(X) \
     else if((X +0) == 0 || CTRYCATCH_NAME(exception_type) == (X +0))
 
-#ifdef finally
-#undef finally
-#endif
 #define finally
 
-#ifdef throw
-#undef throw
-#endif
 #define throw(X,...) \
     CTRYCATCH_NAME(exception_message) = (__VA_ARGS__  +0), longjmp(CTRYCATCH_NAME(exception_env), (X))
 
@@ -79,9 +58,6 @@ extern CTRYCATCH_NAME(exception_types) CTRYCATCH_NAME(exception_type);
 extern char *CTRYCATCH_NAME(exception_message);
 
 // Helper functions
-#ifdef __ctrycatch_exception_message_exists
-#undef __ctrycatch_exception_message_exists
-#endif
 #define __ctrycatch_exception_message_exists (bool)CTRYCATCH_NAME(exception_message)
 
 // Exception types
